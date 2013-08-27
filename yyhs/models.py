2ae92a5a831d4d15ccbf880851162be1c17992ca
LongWei <yilang2007lw@gmail.com>
2013-08-28 00:47:00 +0800

    update models

--- yyhs/models.py
+++ yyhs/models.py
@@ -1,35 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User,Group
 
 VISIABLE_CHOICES = [("all","all"), ("class","class"), ("personal","personal")]
 SEX_CHOICES = [("male", "male"), ("female","female")]
 
 class UserInfo(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User,primary_key=True)
     age = models.IntegerField()
     sex = models.CharField(choices=SEX_CHOICES, default="male", max_length=6)
-    email = models.EmailField(max_length=75)
     mobilephone = models.IntegerField(max_length=13)
     face = models.CharField(max_length=100)
-    jointime = models.DateTimeField(auto_now_add=True)
     industry = models.CharField(max_length=100)
     job = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     hometown = models.CharField(max_length=100)
 
+    class Meta:
+        abstract = True
+
+class Teacher(UserInfo):
+    subject = models.CharField(max_length=20)
+
 class Classes(models.Model):
-    name = models.CharField(max_length=100)
-    graduate = models.DateField()
-    head_teacher = models.CharField(max_length=100)
+    name = models.CharField(max_length=10)
+    year = models.DateField()
+    number = models.IntegerField()
+    head_teacher = models.OneToOneField(Teacher)
     logo = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
+    teachers = models.ManyToManyField(Teacher,related_name="%(app_label)s_%(class)s_related")
 
-class Image(models.Model):
-    name = models.CharField(max_length=100)
-    pubdate = models.DateTimeField(auto_now_add=True)
-    url = models.CharField(max_length=100)
+    def __unicode__(self):
+        return u"classname is %s" % self.name
 
-    class Meta:
-        ordering = ('pubdate',)
+class Student(UserInfo):
+    banji = models.ForeignKey(Classes)
 
 class Album(models.Model):
     name = models.CharField(max_length=100, blank=True, default="album name")
@@ -41,15 +46,34 @@ class Album(models.Model):
     class Meta:
         ordering = ('pubdate',)
 
+class Image(models.Model):
+    album = models.ForeignKey(Album)
+    name = models.CharField(max_length=100)
+    pubdate = models.DateTimeField(auto_now_add=True)
+    url = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('pubdate',)
+
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    body = models.CharField(max_length=1024)
-    category = models.CharField(max_length=10)
+    author = models.ForeignKey(User)
+    content = models.CharField(max_length=1024)
     pubdate = models.DateTimeField(auto_now_add=True)
 
-class Comment(models.Model):
-    reference = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    pubdate = models.DateTimeField(auto_now_add=True)
-    content = models.CharField(max_length=1024)
+    class Meta:
+        abstract = True
+
+class News(Post):
+    title = models.CharField(max_length=100)
+
+class Blog(Post):
+    tag = models.CharField(max_length=10)
+    belong_class = models.IntegerField()
+
+class Notes(Post):
+    target = models.CharField(max_length=10)
+    anonymous = models.IntegerField()
+
+#class Comment(Post):
+#    post = models.ForeignKey(Post)
+
